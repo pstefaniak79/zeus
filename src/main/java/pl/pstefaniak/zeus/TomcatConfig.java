067459e9b35a9dd8d8d8f01a22b59ad90836a55a
Piotr Stefaniak <pstefaniak79@gmail.com>
2020-10-04 16:59:44 +0200

    2

---
+++ src/main/java/pl/pstefaniak/zeus/TomcatConfig.java
@@ -0,0 +1,15 @@
+package pl.pstefaniak.zeus;
+
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig
+        implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableServletWebServerFactory container) {
+        container.setContextPath("/zeus");
+    }
+}
