69b07e75954c57e415d6c64221fd1589174d92b3
Piotr Stefaniak <pstefaniak79@gmail.com>
2020-10-14 21:41:33 +0200

    refactoring

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM openjdk:8-jdk-alpine
+FROM openjdk:8u191-jdk-alpine3.9
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
+EXPOSE 8201
--- src/main/java/pl/pstefaniak/zeus/controller/SelfDiscovery.java
+++ src/main/java/pl/pstefaniak/zeus/controller/SelfDiscovery.java
@@ -3,16 +3,26 @@ package pl.pstefaniak.zeus.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.net.InetAddress;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class SelfDiscovery {
+    AtomicInteger i = new AtomicInteger();
+
     @GetMapping("/ip")
     public String selfDiscovery() {
         try {
             InetAddress ip = InetAddress.getLocalHost();
             String userHome = System.getProperty("user.home");
             String userDir = System.getProperty("user.dir");
+            File file = new File("/zeus_logs/logs_" + i.incrementAndGet());
+            file.getParentFile().mkdirs();
+            FileWriter writer = new FileWriter(file, true);
+            writer.write("hostName/ip=" + ip + "| userHome=" + userHome + "| userDir=" + userDir);
+            writer.close();
             return "hostName/ip=" + ip + "| userHome=" + userHome + "| userDir=" + userDir;
         } catch (Exception e) {
         }
